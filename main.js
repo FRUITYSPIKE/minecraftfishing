7b88af6baf19b0797cb0b5f0099ee02e54a0d2f6
FRUITYSPIKE <145369924+FRUITYSPIKE@users.noreply.github.com>
2024-04-10 09:27:52 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,87 @@
+// Functionize Minecraft Fishing Start Code
+
+//HTML variables
+
+let resultImg = document.getElementById("result-img");
+let numCodSpan = document.getElementById("num-cod");
+let numSalmonSpan = document.getElementById("num-salmon");
+let numTropicalSpan = document.getElementById("num-tropical");
+let numPufferSpan = document.getElementById("num-puffer");
+//Fish count vari
+
+let numCod = 0;
+let numSalmon = 0;
+let numTropical = 0;
+let numPuffer = 0;
+
+// Fish Button Event Listener
+document.getElementById("fish-btn").addEventListener("click", fishBtnClicked);
+
+function fishBtnClicked() {
+  let charcter = document.getElementById("character-select").value;
+  if (charcter === "steve") {
+    //fish with steve
+    //Steve Fish Sim
+    let randNum = Math.random();
+    console.log(randNum);
+    if (randNum < 0.7) {
+      resultImg.src = "img/Raw-Cod.png";
+      numCod++;
+      numCodSpan.innerHTML = numCod;
+    } else if (randNum < 0.9) {
+      resultImg.src = "img/Raw-Salmon.png";
+      numSalmon++;
+      numSalmonSpan.innerHTML = numSalmon;
+    } else if (randNum < 0.95) {
+      resultImg.src = "img/Tropical-Fish.png";
+      numTropical++;
+      numTropicalSpan.innerHTML = numTropical;
+    } else {
+      resultImg.src = "img/PufferFish.png";
+      numPuffer++;
+      numPufferSpan.innerHTML = numPuffer;
+    }
+  } else if (charcter === "alex") {
+    //fish with alex
+    let randNum = Math.random();
+    console.log(randNum);
+    if (randNum < 0.1) {
+      resultImg.src = "img/Raw-Cod.png";
+      numCod++;
+      numCodSpan.innerHTML = numCod;
+    } else if (randNum < 0.2) {
+      resultImg.src = "img/Raw-Salmon.png";
+      numSalmon++;
+      numSalmonSpan.innerHTML = numSalmon;
+    } else if (randNum < 0.5) {
+      resultImg.src = "img/Tropical-Fish.png";
+      numTropical++;
+      numTropicalSpan.innerHTML = numTropical;
+    } else {
+      resultImg.src = "img/PufferFish.png";
+      numPuffer++;
+      numPufferSpan.innerHTML = numPuffer;
+    }
+  } else if (charcter === "villager") {
+    //villager fishing sim
+    let randNum = Math.random();
+    console.log(randNum);
+    if (randNum < 0.25) {
+      resultImg.src = "img/Raw-Cod.png";
+      numCod++;
+      numCodSpan.innerHTML = numCod;
+    } else if (randNum < 0.5) {
+      resultImg.src = "img/Raw-Salmon.png";
+      numSalmon++;
+      numSalmonSpan.innerHTML = numSalmon;
+    } else if (randNum < 0.75) {
+      resultImg.src = "img/Tropical-Fish.png";
+      numTropical++;
+      numTropicalSpan.innerHTML = numTropical;
+    } else {
+      resultImg.src = "img/PufferFish.png";
+      numPuffer++;
+      numPufferSpan.innerHTML = numPuffer;
+    }
+  }
+}
